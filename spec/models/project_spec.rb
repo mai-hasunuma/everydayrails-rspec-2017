7c23e313135a0e61b7c2337b0ba2dbc63b2b7aab
mai-hasunuma <mai.tomimura@gmail.com>
2019-11-05 08:15:35 +0000

    モデルテスト完了

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -49,22 +49,28 @@ RSpec.describe Project, type: :model do
     expect(other_project).to be_valid
   end
 
+  # プロジェクトにはたくさんのメモがついていること
+  it "can have many notes" do
+    project = FactoryBot.create(:project, :with_notes)
+    expect(project.notes.length).to eq 5
+  end
+
   describe "last status" do
     #締切日が過ぎていれば遅延していること
     it "is late when the due date is past today" do
-      project = FactoryBot.create(:project_due_yesterday)
+      project = FactoryBot.create(:project, :due_yesterday)
       expect(project).to be_late
     end
 
     #締切日が今日ならスケジュール通りであること
     it "is on time when the due date is today" do
-      project = FactoryBot.create(:project_due_today)
+      project = FactoryBot.create(:project, :due_today)
       expect(project).to_not be_late
     end
 
     # 締切日が未来ならスケジュール通りであること
     it "is on time when the due date is in the future" do
-      project = FactoryBot.create(:project_due_tomorrow)
+      project = FactoryBot.create(:project, :due_tomorrow)
       expect(project).to_not be_late
     end
   end
